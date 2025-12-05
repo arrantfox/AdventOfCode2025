f23b3aa5453c77209576850c51032bb57fba033e
arrantfox <183362158+arrantfox@users.noreply.github.com>
2025-12-05 22:49:58 +0100

    Add day 5 solution

---
+++ app/Aoc/Day05.hs
@@ -0,0 +1,39 @@
+module Aoc.Day05 (
+    part1,
+    part2,
+)
+where
+import Data.List.Split (splitOn)
+import Control.Monad (join)
+
+type Range = (Integer, Integer)
+type Id = Integer
+
+parse :: [Char] -> ([Range], [Id])
+parse input = let (r:i:_) = map lines $ splitOn "\n\n" input in (map (toTuple . map read . splitOn "-") r, map read i)
+    where toTuple (a:b:_) = (a, b)
+
+inRange ::  Id -> Range ->Bool
+inRange i (min, max) = i >= min && i <= max
+
+isFresh :: [Range] -> Id -> Bool
+isFresh rs i = any (inRange i) rs
+
+part1 :: String -> String
+part1 i = show $ length $ filter (isFresh rs) ids
+    where
+        (rs, ids) = parse i
+
+merge :: Range -> Range -> Maybe Range
+merge (a, b) (x, y)
+    | inRange x (a, b) || inRange y (a, b)= Just (min a x, max b y)
+    | otherwise = Nothing
+
+part2 :: String -> String
+part2 i = show $ allFresh $ iterate mergeFirst rs !! length rs
+    where
+        (rs, _) = parse i
+        mergeFirst (a:b:rs) = maybe (b:mergeFirst (a:rs)) (:rs) (merge a b)
+        mergeFirst [a] = [a]
+        allFresh = sum . map (\(a,b) -> b - a + 1)
+
--- app/Aoc/Days.hs
+++ app/Aoc/Days.hs
@@ -4,10 +4,12 @@ import Aoc.Day01
 import Aoc.Day02
 import Aoc.Day03
 import Aoc.Day04
+import Aoc.Day05
 
 days =
     [ (Aoc.Day01.part1, Aoc.Day01.part2)
     , (Aoc.Day02.part1, Aoc.Day02.part2)
     , (Aoc.Day03.part1, Aoc.Day03.part2)
     , (Aoc.Day04.part1, Aoc.Day04.part2)
+    , (Aoc.Day05.part1, Aoc.Day05.part2)
     ]
