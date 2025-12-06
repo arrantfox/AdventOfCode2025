13997e6574776be491d80eeef4669dcff1a64013
arrantfox <183362158+arrantfox@users.noreply.github.com>
2025-12-06 19:44:43 +0100

    Add day 6 solution

--- app/Aoc/Days.hs
+++ app/Aoc/Days.hs
@@ -5,6 +5,7 @@ import Aoc.Day02
 import Aoc.Day03
 import Aoc.Day04
 import Aoc.Day05
+import Aoc.Day06
 
 days =
     [ (Aoc.Day01.part1, Aoc.Day01.part2)
@@ -12,4 +13,5 @@ days =
     , (Aoc.Day03.part1, Aoc.Day03.part2)
     , (Aoc.Day04.part1, Aoc.Day04.part2)
     , (Aoc.Day05.part1, Aoc.Day05.part2)
+    , (Aoc.Day06.part1, Aoc.Day06.part2)
     ]
